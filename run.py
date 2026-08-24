7b3e8e1784f8a681e186561895775f1478f2a515
Aria <aria.and.idriss@gmail.com>
2026-08-25 01:43:42 +0200

    Reorganize import statements and main entry point

--- run.py
+++ run.py
@@ -1,12 +1,8 @@
 # Importations externes
 import tkinter as tk
-import sys
-from pathlib import Path
 
 # Importation interne
 from app.API import API
-
-sys.path.append(str(Path(__file__).parent))
 
 # Point d'entrée du logiciel
 if __name__ == "__main__":
