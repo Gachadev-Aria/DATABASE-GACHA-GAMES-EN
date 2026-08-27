1ac5ac9f0d29862ed1991de47a78ba7996746242
Aria <aria.and.idriss@gmail.com>
2026-08-27 11:20:18 +0200

    Add 'Gacha Want' to list_games

--- app/static/Listes.py
+++ app/static/Listes.py
@@ -4,5 +4,5 @@ colonnes_sqlGG = ["CharacterId", "CharacterName", "Game", "CharacterImage"]
 colonnes_G = ["Id", "Nom", "Image"]
 colonnes_sqlG = ["CharacterId", "CharacterName", "CharacterImage"]
 
-list_games = ["Gacha Club", "Gacha Life 2", "Gacha Nebula v1.6", "Gacha Plus", "Gacha Luminal", "Gacha Realms", "Gacha Ultra", "Gacha Life", "Gachaverse"]
+list_games = ["Gacha Club", "Gacha Life 2", "Gacha Nebula v1.6", "Gacha Plus", "Gacha Luminal", "Gacha Realms", "Gacha Ultra", "Gacha Life", "Gachaverse", "Gacha Want"]
 liste_all = ["Gacha Games", "Gacha Life", "Gacha Club", "Gacha Life 2", "Gacha Nebula v1.6"]
