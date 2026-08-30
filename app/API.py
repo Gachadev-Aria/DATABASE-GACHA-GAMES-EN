20d8f18fbb2f5ba763d8845b55c017c3bbf54ae1
Aria <aria.and.idriss@gmail.com>
2026-08-30 23:15:45 +0200

    Update API.py to include new JSON path
    Added a new path for JSON data and updated methods to handle it.

--- app/API.py
+++ app/API.py
@@ -11,7 +11,7 @@ from app.Fonctions import new_tableau, new_Notebook, remplir_tableau, choisir_co
 from app.Fonctions import on_double_clic_principal, definir_police_color
 from app.FontChooserDialog import FontChooserDialog
 from app.GestionDB import DB
-from app.config import dest_para_json_path
+from app.config import dest_para_json_path, dest_json_path
 
 class API():
     """Classe principale du logciel: l'interface."""
@@ -26,6 +26,7 @@ class API():
         self.DataBase = DB()
         self.FontChooserDialog = FontChooserDialog
         self.path1 = dest_para_json_path
+        self.path2 = dest_json_path
         self.bg_color = extraire_texte_depuis_json(self.path1, "bg_color")
         self.btn_color = extraire_texte_depuis_json(self.path1, "btn_color")
         self.police = extraire_texte_depuis_json(self.path1, "police et taille")
@@ -167,12 +168,11 @@ class API():
                         values = tableau.item(item, "values")
                         if values[0] == int(a) or values[0] == str(a):
                             tableau.delete(item)
-            
-                path = os.path.join(os.path.dirname(__file__), "fichier_code", "codes.json")
-                with open(path, "r", encoding="utf-8") as f:
+
+                with open(self.path2, "r", encoding="utf-8") as f:
                     codes = json.load(f)
                 del codes[values[0]]
-                with open(path, "w", encoding="utf-8") as f:
+                with open(self.path2, "w", encoding="utf-8") as f:
                     json.dump(codes, f, ensure_ascii=False, indent=4)
                 used_ids.discard(int(a))
                 
@@ -218,8 +218,8 @@ class API():
             if not ligne_select:
                 messagebox.showerror("Try again", "Select an OC whose code need to be copied.", icon='error')
                 return   
-            path = os.path.join(os.path.dirname(__file__), "fichier_code", "codes.json")  
-            with open(path, "r", encoding="utf-8") as f:
+            
+            with open(self.path2, "r", encoding="utf-8") as f:
                 codes = json.load(f)
             code = codes.get(a)
             if code:
